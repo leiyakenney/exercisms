af31d21d2c46cd3b195f531e958375eff37271a3
Leiya Kenney <leiya.kenney@gmail.com>
2020-02-28 16:22:29 -0700

    Include apostrophes in allowable characters

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -5,7 +5,7 @@ class Phrase
 
   def word_count
     counts = Hash.new(0)
-    @phrase.downcase.gsub(/[^0-9A-Za-z ,]/,'').split(/[\s,]+/).map do |word|
+    @phrase.downcase.gsub(/[^0-9A-Za-z ,']/,'').split(/[\s,]+/).map do |word|
       counts[word] += 1
     end
     counts
