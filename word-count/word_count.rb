4c829d90e78f1a4901976168eacf8b6d406d3a13
Leiya Kenney <leiya.kenney@gmail.com>
2020-03-02 10:52:07 -0700

    Remove commented out old word_count method

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -3,14 +3,6 @@ class Phrase
     @phrase = phrase
   end
 
-  # def word_count
-  #   counts = Hash.new(0)
-  #   @phrase.downcase.gsub(/[^0-9A-Za-z ,']/,'').split(/[\s,]+/).map do |word|
-  #     counts[word] += 1
-  #   end
-  #   counts
-  # end
-
   def word_count
     words.each_with_object(Hash.new(0)) do |word, freq|
       freq[word] += 1
