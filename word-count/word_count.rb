f16c16c4e8806d4efda9fe93dce18da44d3f29e5
Leiya Kenney <leiya.kenney@gmail.com>
2020-03-02 10:51:20 -0700

    Exclude single quotes, include apostrophes

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -3,11 +3,23 @@ class Phrase
     @phrase = phrase
   end
 
+  # def word_count
+  #   counts = Hash.new(0)
+  #   @phrase.downcase.gsub(/[^0-9A-Za-z ,']/,'').split(/[\s,]+/).map do |word|
+  #     counts[word] += 1
+  #   end
+  #   counts
+  # end
+
   def word_count
-    counts = Hash.new(0)
-    @phrase.downcase.gsub(/[^0-9A-Za-z ,']/,'').split(/[\s,]+/).map do |word|
-      counts[word] += 1
+    words.each_with_object(Hash.new(0)) do |word, freq|
+      freq[word] += 1
     end
-    counts
   end
+
+  private
+  def words
+    @phrase.downcase.scan(/\b[\w']+\b/)
+  end
+
 end
